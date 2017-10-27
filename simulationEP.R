905a5c2da591264454191cb25719dc40262f5780
atwgao <gao.isc@gmail.com>
2017-10-27 21:19:49 +0200

    changed RevEPD to be generic

--- simulationEP.R
+++ simulationEP.R
@@ -11,7 +11,7 @@ library(methods)
 source("./headers/Raux.R")
 source("./headers/Distributions.R")
 source("./headers/auxillary.R")
-source("./headers/function_inference.R")
+source("./headers/function_inference_pareto.R")
 source("./headers/parametric_functions_Inference.R")
 
 simul.r <- function(r,n,m,babu,sigma=NA,xi=NA,empiric=FALSE,seed=1){
@@ -19,23 +19,28 @@ simul.r <- function(r,n,m,babu,sigma=NA,xi=NA,empiric=FALSE,seed=1){
   #x <- rgamma(n,1,1)
   #x <- rlnorm(n,0,1)
   #x<-runif(n)
-  x <- rbeta(n,2,2)
-  #x <- ReIns::rburr(n,alpha=1/xi,rho=-1)
-  #x <- rweibull(n,10,0.5)
+  #x <- rbeta(n,2,2)
+  x <- ReIns::rburr(n,alpha=1/xi,rho=-1)
+  #x <- rtburr(n,4,-1,1,0.5)
   #x <- ReIns::rfrechet(n,1/xi)
+  #x<-model1(n,2,1)
+  #x<-model2(n,3/2)
+  #x<-model3(n,0.5,3)
+  #x <- model4(n,xi)
   #x<- abs(rt(n,1/xi))
+  #x <- rgpd(n,shape=xi)
   #x <- exp(rgamma(n,4,2))
   x <- x[x>0]
   bb<-m
-  return(list(EGP1=egpd_s(m=m,x,model ="GP",omega=0,badu=babu)$gamma,EGP2=egpd_s(m=m,x,model ="GP",omega=100,badu=babu)$gamma,
-              H=ReIns:::GPDmle(x)$gamma[bb:(n-1)],EPD=Moment(x)$gamma[bb:(n-1)]))
+  return(list(EGP1=egpd_s(m=m,x,model ="Pa",omega=0,badu=babu)$gamma,EGP2=egpd_s(m=m,x,model ="Pa",omega=0.1,badu=babu)$gamma,
+              H=ReIns:::Hill(x)$gamma[bb:(n-1)],EPD=RevEPD(x)[bb:(n-1)]))
 }
 
-nsim<-40
-m=1
-babu <- TRUE
-n<-100
-xi<--0.5
+nsim<-64
+m=3
+babu <- F
+n<-50
+xi<-0.5
 t1<-system.time(res <- mclapply(c(1:nsim),simul.r,n=n,m=ifelse(babu==T,1,m),babu=babu,sigma=sigma,xi=xi,seed=1,mc.cores=detectCores()))
 
 return.arg <- function(l,i){
@@ -65,7 +70,7 @@ var_EPD<-apply(return.arg2(lapply(res,return.arg,i=4)),1,var,na.rm=TRUE)
 K<-m:(n-1)
 #pdf(file="EP_frechet_evi_m32.pdf",width = 6, height = 6)
 #par(mar=c(5,5,5,2))
-plot(K,EGP1,type="l",xlab="K",ylab=bquote(EVI),ylim=c(-1.5,xi+0.5),lwd=2.5,lty=1)
+plot(K,EGP1,type="l",xlab="K",ylab=bquote(EVI),ylim=c(-1,1),lwd=2.5,lty=1)
 lines(K,EGP2,col=4,lwd=2.5,lty=1)
 lines(K,H,col=8,lwd=2,lty=2)
 lines(K,EPD,col="darkred",lwd=3,lty=1)
@@ -76,7 +81,7 @@ abline(h=xi)
 
 #pdf(file="EP_frechet_rmse_m32.pdf",width = 6, height = 6)
 #par(mar=c(5,5,5,2))
-plot(K,rmse_EGP1,type="l",xlab="K",ylab=bquote(RMSE),ylim=c(-0.3,xi+0.3),lwd=2.5,lty=1)
+plot(K,rmse_EGP1,type="l",xlab="K",ylab=bquote(RMSE),ylim=c(-0.3,1),lwd=2.5,lty=1)
 lines(K,rmse_EGP2,col=4,lwd=2.5,lty=1)
 lines(K,rmse_H,col=8,lwd=2,lty=2)
 lines(K,rmse_EPD,col="darkred",lwd=2,lty=1)
