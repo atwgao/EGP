01ab1a02c67a862f4df9d05cc449a6b882d06140
Gao <2007052190@ufs4life.ac.za>
2017-10-28 15:58:03 +0200

    Parellel m
    running this in parallel

--- different_ms.R
+++ different_ms.R
@@ -8,16 +8,25 @@ source("./headers/auxillary.R")
 source("./headers/function_inference_pareto.R")
 source("./headers/parametric_functions_Inference.R")
 
-n<-100
+n<-500
 xi<-0.5
 x <- rburr(n,1/xi,-1)
+ms<-16
 
+out<-function(i){
+  return(egpd_s(m=i,x,model ="Pa",omega=0,badu=F)$gamma)
+}
 
-plot(RevEPD(x),col=1,lwd=3,type="l",ylim=c(0,2))
-Hill(x,add=T,lwd="orange")
+estimators<-data.frame(matrix(NA,ncol=ms,nrow=(n-1)))
+plot(estimators[,1]<-RevEPD(x)[1:(n-1)],col=1,lwd=3,type="l",ylim=c(0,2),ylab="EVI",main="Estimates of the Extreme value index",xlab="K")
 #EPD(x,add=T,direct=T,col=1,lwd=5)
 abline(h=xi)
-for(i in 1:1){
+result<-mclapply(c(1:ms),out,mc.cores=detectCores())
+for(i in 2:ms){
 K<-i:(n-1)
-lines(K,egpd_s(m=i,x,model ="Pa",omega=0,badu=F)$gamma,col=i+1,lwd=2)
+lines(K,(estimators[K,i]<-result[[i]]),col=i+1,lwd=2)
+print(i)
 }
+
+
+#plot(estimators[,1],col=1,lwd=3,type="l",ylim=c(0,2),ylab="EVI",main="Estimates of the Extreme value index",xlab="K")
