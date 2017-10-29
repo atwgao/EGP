01be7d4bd4d4db3220b95d42af397f9962b745f4
atwgao <gao.isc@gmail.com>
2017-10-29 13:14:19 +0200

    results stored in estimator dataframe

--- different_ms.R
+++ different_ms.R
@@ -8,7 +8,7 @@ source("./headers/auxillary.R")
 source("./headers/function_inference_pareto.R")
 source("./headers/parametric_functions_Inference.R")
 
-n<-500
+n<-100
 xi<-0.5
 
 x <- rburr(n,1/xi,-1)
@@ -18,24 +18,37 @@ out<-function(i,o){
   return(egpd_s(m=i,x,model ="Pa",omega=o,badu=F,mfun=1)$gamma)
 }
 
-estimators<-data.frame(matrix(NA,ncol=ms,nrow=(n-1)))
-#EPD(x,add=T,direct=T,col=1,lwd=5)
-Hill(x,plot=T,ylim=c(0,1))
-lines(RevEPD(x,-0.5)[1:(n-1)],col=1,lwd=3)
-abline(h=xi)
+estimators<-data.frame(matrix(NA,ncol=ms+5,nrow=(n-1)))
 
 result<-mclapply(c(1:ms),out,o=0.01,mc.cores=detectCores())
-for(i in 1:ms){
-K<-i:(n-1)
-Sys.sleep(1)
-lines(K,(estimators[K,i]<-result[[i]]),col=i+1,lwd=2)
-print(i)
-}
 
 K<-1:(n-1)
 M<-n/log(n)
-mfun1=ceiling(K/log(K))
-mfun2=ceiling(M*(K/n)^3)
-bads<-egpd_s(m=1,x,model ="Pa",omega=0.01,badu=T,mfun=mfun1)$gamma
-lines(bads,lwd=3,col="steelblue")
+estimators[,ms+1]<-egpd_s(m=1,x,model ="Pa",omega=0.01,badu=T,mfun=ceiling(K/log(K)))$gamma
+estimators[,ms+2]<-egpd_s(m=1,x,model ="Pa",omega=0.01,badu=T,mfun=ceiling(M*(K/n)^1))$gamma
+estimators[,ms+3]<-egpd_s(m=1,x,model ="Pa",omega=0.01,badu=T,mfun=ceiling(M*(K/n)^2))$gamma
+estimators[,ms+4]<-egpd_s(m=1,x,model ="Pa",omega=0.01,badu=T,mfun=ceiling(M*(K/n)^3))$gamma
+estimators[,ms+5]<-egpd_s(m=1,x,model ="Pa",omega=0.01,badu=T,mfun=ceiling(M*(K/n)^4))$gamma
+estimators[,ms+6]<-RevEPD(x,-1)[1:(n-1)]
+estimators[,ms+7]<-RevEPD(x,-2)[1:(n-1)]
+estimators[,ms+8]<-RevEPD(x,-3)[1:(n-1)]
+estimators[,ms+9]<-RevEPD(x,-4)[1:(n-1)]
+
+
+for(i in 1:ms){
+K<-i:(n-1)
+#Sys.sleep(1)
+estimators[K,i]<-result[[i]]
+#lines(K,result[[i]],col=i+1,lwd=2)
+print(i)
+}
+
+#EPD(x,add=T,col=1,lwd=5)
+Hill(x,plot=T,ylim=c(0,1))
+lines(estimators[,ms+8],col=2,lwd=3)
+abline(h=xi)
+lines(estimators[,ms+3],lwd=4,col="red4")
 # abline(h=xi)
+
+plot(mfun1,type="l",lwd=4)
+lines(mfun2,col="red4",lwd=4)
